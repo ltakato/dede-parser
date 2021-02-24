0e5a1f4886ed4cdabb0a62c3d5fc2a643a0d1afd
Gustavo Fontes <gustavo.ferrazfontes@gmail.com>
2021-02-24 17:34:29 -0300

    feature: adição de projeto de api

---
+++ dede-parser/Api/Controllers/ParseController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ParserController : ControllerBase
+    {
+        private readonly ILogger<ParserController> _logger;
+
+        public ParserController(ILogger<ParserController> logger)
+        {
+            _logger = logger;
+        }
+
+    }
+}
